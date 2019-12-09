58a6da6d4d0e3f9b94ad5227d1708d97b2600aeb
annisahasnul17 <57389062+annisahasnul17@users.noreply.github.com>
2019-12-09 22:18:27 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PeKaeL
                 </div>
 
                 <div class="links">
