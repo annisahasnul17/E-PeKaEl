1205f940f1d6c4e1be2ac9b637c46f828827d080
annisahasnul17 <57389062+annisahasnul17@users.noreply.github.com>
2019-12-16 18:06:44 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,7 @@
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
+                background-image: url("http://localhost/pekaels/resources/img/bgs.jpg");
             }
 
             .full-height {
@@ -45,13 +46,14 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 100px;
+                color: white;
             }
 
             .links > a {
-                color: #636b6f;
+                color: white;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 20px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -81,18 +83,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PeKaeL
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                
+                    
+                    <a href="https://laravel-news.com">MAKANAN</a>
+                    <a href="https://blog.laravel.com">MINUMAN</a>
+                    <a href="https://nova.laravel.com">PAKAIAN</a>
+                    <a href="https://forge.laravel.com">AKSESORIS</a>
                 </div>
             </div>
         </div>
